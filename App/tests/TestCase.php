b5e6f210a7cb741a644ccb6ab854aae2c63a32ae
AprendizPR <jonasfg1319@hotmail.com>
2021-09-07 11:58:56 -0300

    Update TestCase.php

--- App/tests/TestCase.php
+++ App/tests/TestCase.php
@@ -8,11 +8,11 @@ class TestCase{
 
     function test_calculo_tempo(){
 
-    	$tempo = array(
-            "horas_totais" => 0,
-            "minutos_totais" => 2,
-            "segundos_totais" => 3
-         );
+            	$tempo = array(
+            	   "horas_totais" => 0,
+            	    "minutos_totais" => 2,
+            	    "segundos_totais" => 3
+         	);
 
 		$time = new Tempo("00","01","52");
 		$time->calculaTempo($tempo);
